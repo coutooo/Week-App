79ba5679dc0bce228c0b21e43ad287fb8158ee5c
Couto <manuelcouto10@ua.pt>
2022-04-06 15:37:31 +0100

    start login

--- week/lib/main.dart
+++ week/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'feed.dart';
+import 'Screens/LoginForm.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Week App'),
+      home: LoginForm(),
     );
   }
 }
