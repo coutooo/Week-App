cc3cc8008497cbc14590c134214b3a8affc7b9ec
Rodrigo Martins <rodrigomartins@ua.pt>
2022-04-28 19:05:10 +0100

    add post done

--- week/lib/Screens/add_post_screen.dart
+++ week/lib/Screens/add_post_screen.dart
@@ -25,6 +25,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
   User user = UserPreferences.myUser;
   Future<SharedPreferences> _pref = SharedPreferences.getInstance();
   final _conUserId = TextEditingController();
+  var photoID;
 
   File? img;
 
@@ -35,15 +36,17 @@ class _AddPostScreenState extends State<AddPostScreen> {
   @override
   void initState() {
     super.initState();
-    getUserData();
     dbHelper = DbHelper.instance;
+    getUserData();
   }
 
   Future<void> getUserData() async {
     final SharedPreferences sp = await _pref;
-
+    int id = await dbHelper.getLastPhotoID();
+    print(id.toString());
     setState(() {
       _conUserId.text = sp.getString("user_id")!;
+      photoID = id.toInt() + 1;
     });
   }
 
@@ -87,6 +90,126 @@ class _AddPostScreenState extends State<AddPostScreen> {
 
     dbHelper.insertPublication(publication);
     print('Publication published');
+  }
+
+  final listKey = GlobalKey<AnimatedListState>();
+  final List<Clothing> items = [];
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void insertItem(String uid, String photoid, String date, String clothType,
+      String brand, String season, String store, String color) {
+    final newIndex = items.length;
+
+    final newItem = Clothing(
+        user_id: uid,
+        photoID: photoid,
+        date: date,
+        clothType: clothType,
+        brand: brand,
+        season: season,
+        store: store,
+        color: color);
+    items.insert(newIndex, newItem);
+    listKey.currentState!.insertItem(newIndex);
+  }
+
+  void removeItem(int index) {
+    final removedItem = items[index];
+    items.removeAt(index);
+    listKey.currentState!.removeItem(
+        index,
+        (context, animation) => ListItemWidget(
+            item: removedItem, animation: animation, onClicked: () {}));
+  }
+
+  Future<void> _showMyDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        final TextEditingController _textEditingControllerType =
+            TextEditingController();
+        final TextEditingController _textEditingControllerBrand =
+            TextEditingController();
+        final TextEditingController _textEditingControllerSeason =
+            TextEditingController();
+        final TextEditingController _textEditingControllerColor =
+            TextEditingController();
+        final TextEditingController _textEditingControllerStore =
+            TextEditingController();
+
+        return AlertDialog(
+          title: const Text('Add a clothing piece'),
+          content: SingleChildScrollView(
+              child: Form(
+            key: _formKey,
+            child: ListBody(
+              children: [
+                TextFormField(
+                  controller: _textEditingControllerType,
+                  validator: (value) {
+                    return value!.isNotEmpty ? null : "Invalid Field";
+                  },
+                  decoration: InputDecoration(hintText: "Type"),
+                ),
+                TextFormField(
+                  controller: _textEditingControllerBrand,
+                  validator: (value) {
+                    return value!.isNotEmpty ? null : "Invalid Field";
+                  },
+                  decoration: InputDecoration(hintText: "Brand"),
+                ),
+                TextFormField(
+                  controller: _textEditingControllerSeason,
+                  validator: (value) {
+                    return value!.isNotEmpty ? null : "Invalid Field";
+                  },
+                  decoration: InputDecoration(hintText: "Season"),
+                ),
+                TextFormField(
+                  controller: _textEditingControllerColor,
+                  validator: (value) {
+                    return value!.isNotEmpty ? null : "Invalid Field";
+                  },
+                  decoration: InputDecoration(hintText: "Color"),
+                ),
+                TextFormField(
+                  controller: _textEditingControllerStore,
+                  validator: (value) {
+                    return value!.isNotEmpty ? null : "Invalid Field";
+                  },
+                  decoration: InputDecoration(hintText: "Store"),
+                ),
+              ],
+            ),
+          )),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              child: const Text('Add'),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  final date = DateTime.now();
+                  insertItem(
+                      _conUserId.text,
+                      photoID.toString(),
+                      date.toString(),
+                      _textEditingControllerType.text,
+                      _textEditingControllerBrand.text,
+                      _textEditingControllerSeason.text,
+                      _textEditingControllerStore.text,
+                      _textEditingControllerColor.text);
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -181,7 +304,14 @@ class _AddPostScreenState extends State<AddPostScreen> {
                 )),
             Container(
               height: 400,
-              child: listWidget(context),
+              child: AnimatedList(
+                  key: listKey,
+                  initialItemCount: items.length,
+                  itemBuilder: (context, index, animation) => ListItemWidget(
+                        item: items[index],
+                        animation: animation,
+                        onClicked: () => removeItem(index),
+                      )),
               width: double.infinity,
               decoration: BoxDecoration(
                   color: Colors.white, borderRadius: BorderRadius.circular(25)),
@@ -189,12 +319,49 @@ class _AddPostScreenState extends State<AddPostScreen> {
           ],
         ),
       ),
+      floatingActionButton: ExpandableFab(
+        distance: 112.0,
+        children: [
+          ActionButton(
+            onPressed: () {
+              _showMyDialog(context);
+            },
+            icon: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+          ActionButton(
+            onPressed: () => pickImage(ImageSource.gallery),
+            icon: const Icon(Icons.insert_photo),
+          ),
+          ActionButton(
+            onPressed: () => pickImage(ImageSource.camera),
+            icon: const Icon(Icons.camera),
+          ),
+          ActionButton(
+            onPressed: () {
+              if (isButtonClickable) {
+                publish();
+                Navigator.pop(context);
+              } else {
+                const snackBar = SnackBar(
+                  content: Text('Please select a photo'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+            },
+            icon: const Icon(Icons.done),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget listWidget(BuildContext context) {
+  Widget listWidget(BuildContext context, String uid, int photoID) {
     final listKey = GlobalKey<AnimatedListState>();
     final List<Clothing> items = [];
+    GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
     void insertItem(String uid, String photoid, String date, String clothType,
         String brand, String season, String store, String color) {
@@ -222,6 +389,96 @@ class _AddPostScreenState extends State<AddPostScreen> {
               item: removedItem, animation: animation, onClicked: () {}));
     }
 
+    Future<void> _showMyDialog() async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          final TextEditingController _textEditingControllerType =
+              TextEditingController();
+          final TextEditingController _textEditingControllerBrand =
+              TextEditingController();
+          final TextEditingController _textEditingControllerSeason =
+              TextEditingController();
+          final TextEditingController _textEditingControllerColor =
+              TextEditingController();
+          final TextEditingController _textEditingControllerStore =
+              TextEditingController();
+
+          return AlertDialog(
+            title: const Text('Add a clothing piece'),
+            content: SingleChildScrollView(
+                child: Form(
+              key: _formKey,
+              child: ListBody(
+                children: [
+                  TextFormField(
+                    controller: _textEditingControllerType,
+                    validator: (value) {
+                      return value!.isNotEmpty ? null : "Invalid Field";
+                    },
+                    decoration: InputDecoration(hintText: "Type"),
+                  ),
+                  TextFormField(
+                    controller: _textEditingControllerBrand,
+                    validator: (value) {
+                      return value!.isNotEmpty ? null : "Invalid Field";
+                    },
+                    decoration: InputDecoration(hintText: "Brand"),
+                  ),
+                  TextFormField(
+                    controller: _textEditingControllerSeason,
+                    validator: (value) {
+                      return value!.isNotEmpty ? null : "Invalid Field";
+                    },
+                    decoration: InputDecoration(hintText: "Season"),
+                  ),
+                  TextFormField(
+                    controller: _textEditingControllerColor,
+                    validator: (value) {
+                      return value!.isNotEmpty ? null : "Invalid Field";
+                    },
+                    decoration: InputDecoration(hintText: "Color"),
+                  ),
+                  TextFormField(
+                    controller: _textEditingControllerStore,
+                    validator: (value) {
+                      return value!.isNotEmpty ? null : "Invalid Field";
+                    },
+                    decoration: InputDecoration(hintText: "Store"),
+                  ),
+                ],
+              ),
+            )),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                child: const Text('Add'),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    final date = DateTime.now();
+                    insertItem(
+                        uid,
+                        photoID.toString(),
+                        date.toString(),
+                        _textEditingControllerType.text,
+                        _textEditingControllerBrand.text,
+                        _textEditingControllerSeason.text,
+                        _textEditingControllerStore.text,
+                        _textEditingControllerColor.text);
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Scaffold(
       body: AnimatedList(
           key: listKey,
@@ -235,8 +492,9 @@ class _AddPostScreenState extends State<AddPostScreen> {
         distance: 112.0,
         children: [
           ActionButton(
-            onPressed: () => insertItem('10', '11', 'asd', 'top', 'zara',
-                'spring', 'zara aveiro', 'white'),
+            onPressed: () {
+              _showMyDialog();
+            },
             icon: const Icon(
               Icons.add,
               color: Colors.white,
